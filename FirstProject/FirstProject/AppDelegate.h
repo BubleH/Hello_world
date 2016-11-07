f5e32e77fe61690cdde29db40340481f7dbf32f0
xiuleid <xiulei@whut.edu.cn>
2016-11-07 18:45:03 +0800

    添加了工程

---
+++ FirstProject/FirstProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstProject
+//
+//  Created by xiulei on 11/7/16.
+//  Copyright © 2016 xiulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstProject/FirstProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstProject
+//
+//  Created by xiulei on 11/7/16.
+//  Copyright © 2016 xiulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
